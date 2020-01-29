ab68270ba7b09b48c76c7c17f52a6610b006df73
Cloneable <1573652017@qq.com>
2020-01-29 11:26:12 +0800

    extract constants

--- src/test/java/com/gildedrose/TexttestFixture.java
+++ src/test/java/com/gildedrose/TexttestFixture.java
@@ -15,14 +15,14 @@ public class TexttestFixture {
 
         Item[] items = new Item[]{
                 new Item("+5 Dexterity Vest", 10, 20), //
-                new Item("Aged Brie", 2, 0), //
+                new Item(GildedRose.AGED, 2, 0), //
                 new Item("Elixir of the Mongoose", 5, 7), //
-                new Item("Sulfuras, Hand of Ragnaros", 0, 80), //
-                new Item("Sulfuras, Hand of Ragnaros", -1, 80),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 15, 20),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 10, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 5, 49),
-                new Item("Backstage passes to a TAFKAL80ETC concert", 1, 20),
+                new Item(GildedRose.SULFURAS, 0, 80), //
+                new Item(GildedRose.SULFURAS, -1, 80),
+                new Item(GildedRose.BACKSTAGE, 15, 20),
+                new Item(GildedRose.BACKSTAGE, 10, 49),
+                new Item(GildedRose.BACKSTAGE, 5, 49),
+                new Item(GildedRose.BACKSTAGE, 1, 20),
                 // this conjured item does not work properly yet
                 new Item("Conjured Mana Cake", 3, 6)};
 
